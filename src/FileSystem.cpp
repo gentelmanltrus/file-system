d798d31486c876d1cfe869a5184d3dfea6930ca0
Aleksandr Samonov <aleksandr.samonov@mif.stud.vu.lt>
2026-05-03 15:29:00 +0300

    10. Conflicts between branches resolved, mkdir and touch functions added, ls in progress

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -9,15 +9,30 @@ FileSystem::FileSystem()
 
 void FileSystem::mkdir(const std::string &name)
 {
-  // must be defined
+  if (!current)
+    throw std::runtime_error("No current directory");
+  if (current->contains(name))
+        throw std::runtime_error("Directory or file already exists");
+  std::shared_ptr<Directory> newDir = std::make_shared<Directory>(name);
+  current->addItem(newDir);
 }
 
 void FileSystem::touch(const std::string &name)
 {
-  // must be defined
+  if (!current)
+    throw std::runtime_error("No current directory");
+
+  if (current->contains(name))
+    throw std::runtime_error("File already exists");
+
+  std::shared_ptr<File> file = std::make_shared<File>(name);
+  current->addItem(file);
 }
 
 void FileSystem::ls() const
 {
-  // must be defined
+  if (!current)
+    throw std::runtime_error("No current directory");
+
+  current->listItems();
 }
